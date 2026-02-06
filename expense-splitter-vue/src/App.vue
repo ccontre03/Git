2e725028ff0db013ddca49117c828bc8fd291961
ccontre03 <christophercontreras23@gmail.com>
2026-02-05 22:30:52 -0600

    Added card changed name

--- expense-splitter-vue/src/App.vue
+++ expense-splitter-vue/src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
   import Header from './components/Header.vue'
+  import Card from './components/Card.vue'
 </script>
 
 <template>
   <Header/>
-
-
-    <main class="card">
-        <form id="personForm" class="rowForm">
-            <input id="personInput" type="text" placeholder="Add person name" />
-            <button>Add Person</button>
-        </form>
+  <Card>    
+    <form id="personForm" class="rowForm">
+        <input id="personInput" type="text" placeholder="Add person name" />
+        <button>Add Person</button>
+    </form>
 
         <form id="expenseForm" class="rowForm">
             <input id="descInput" type="text" placeholder="Expense Description" />
@@ -45,9 +44,30 @@
             <ul id="summaryList" class="list">
             </ul>
         </section>
-    </main>
+    </Card>
   </template>
 
 <style scoped>
+.rowForm {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 12px;
 
+}
+
+input, select {
+    flex: 1;
+    padding: 10px;
+    border-radius: 10px;
+    border: 1px solid #34025e;
+}
+
+button {
+    padding: 10px 14px;
+    border-radius: 10px;
+    border: none;
+    background: #09b89b;
+    color: #774ab8;
+    cursor: pointer;
+}
 </style>
---
+++ expense-splitter-vue/src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="card">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.card {
+    width: min(600px, 100%);
+    background: #c60a87;
+    padding: 18px;
+    border-radius: 16px;
+    box-shadow: 0 10px 24px rgba(79, 70, 229, .2);
+}
+</style>
