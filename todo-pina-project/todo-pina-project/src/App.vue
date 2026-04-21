fac0387faa5d58266c1effbc1ec8b887e2707774
ccontre03 <christophercontreras23@gmail.com>
2026-04-21 11:36:35 -0500

    cleaned/setup

--- todo-pina-project/todo-pina-project/src/App.vue
+++ todo-pina-project/todo-pina-project/src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="app">
+    <h1>My To-Do List</h1>
+    <p>Pending Tasks, Completed Tasks, Tomorrow's Tasks</p>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.app {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-family: Arial, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  margin-bottom: 0.5rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+p {
+  color: #555;
 }
 </style>
